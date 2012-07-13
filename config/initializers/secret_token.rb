f46b7d20cb03e21f7d49eab40ca253a4b0424022
Travis Briggs <audiodude@gmail.com>
2012-07-13 00:21:14 +0000

    Initial commit, with mysql2 gem and thin, removing public/index.html and rails.png

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artchart::Application.config.secret_token = '8b462822f9a855f7ac1762e6dfab98ffd52706dce59348085fcecb73bccbe46e7547d89208e95dc8a508055c2432a087f25162fc0e6849a0418ae7a6296bf05f'
